7c4954959772254148fb3e0c96b12a1b9cc2a617
Ross Golder <ross@golder.org>
2023-09-02 20:42:21 +0700

    Add ability to dump Discord payload to file for collection as output artifact.

--- main.go
+++ main.go
@@ -18,30 +18,45 @@ type NodeInfo struct {
 }
 
 type DiscordEmbed struct {
-	Title       string                 `json:"title"`
-	Description string                 `json:"description"`
-	Color       int                    `json:"color"`
-	Fields      []map[string]string    `json:"fields"`
+	Title       string              `json:"title"`
+	Description string              `json:"description"`
+	Color       int                 `json:"color"`
+	Fields      []map[string]string `json:"fields"`
 }
 
-func sendDiscordMessage(webhookURL string, embeds []DiscordEmbed) {
+func prepareDiscordMessage(embeds []DiscordEmbed) ([]byte, error) {
 	payload := map[string][]DiscordEmbed{"embeds": embeds}
 	jsonPayload, err := json.Marshal(payload)
 	if err != nil {
-		fmt.Println("Error marshalling JSON:", err)
-        	os.Exit(1)
+		return nil, fmt.Errorf("error marshalling JSON: %s", err)
 	}
 
+	payloadFile := os.Getenv("DISCORD_PAYLOAD_TO_FILE")
+	if payloadFile != "" {
+		file, err := os.Create(payloadFile)
+		if err != nil {
+			fmt.Printf("error creating payload file: %s", err)
+		}
+		_, err = file.Write(jsonPayload)
+		if err != nil {
+			fmt.Printf("error writing to payload file: %s", err)
+		}
+	}
+
+	return jsonPayload, nil
+}
+
+func sendDiscordMessage(webhookURL string, jsonPayload []byte) error {
 	resp, err := http.Post(webhookURL, "application/json", bytes.NewBuffer(jsonPayload))
 	if err != nil {
-		fmt.Println("Error sending request:", err)
-        	os.Exit(1)
+		return fmt.Errorf("error sending request: %s", err)
 	}
 
 	if resp.StatusCode != http.StatusNoContent {
-		fmt.Println("Failed to send message, Status:", resp.Status)
-        	os.Exit(1)
+		return fmt.Errorf("failed to send message, Status: %s", resp.Status)
 	}
+
+	return nil
 }
 
 func main() {
@@ -50,7 +65,7 @@ func main() {
 
 	if webhookURL == "" || workflowStatus == "" {
 		fmt.Println("Required environment variables missing.")
-		return
+		os.Exit(1)
 	}
 
 	var nodes []NodeInfo
@@ -63,7 +78,7 @@ func main() {
 		embed := DiscordEmbed{
 			Title:       "Node Failure Information",
 			Description: fmt.Sprintf("Node: %s", node.DisplayName),
-			Color:       16711680,  // Bright red
+			Color:       16711680, // Bright red
 			Fields: []map[string]string{
 				{"name": "Message", "value": node.Message},
 				{"name": "Template", "value": node.TemplateName},
@@ -75,5 +90,14 @@ func main() {
 		embeds = append(embeds, embed)
 	}
 
-	sendDiscordMessage(webhookURL, embeds)
+	payload, err := prepareDiscordMessage(embeds)
+	if err != nil {
+		fmt.Printf("Error preparing Discord message: %s", err)
+		os.Exit(1)
+	}
+	err = sendDiscordMessage(webhookURL, payload)
+	if err != nil {
+		fmt.Printf("Error sending Discord message: %s", err)
+		os.Exit(1)
+	}
 }
